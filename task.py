04d78cd638804c9af163ee1d16afc10c545861f5
Jahid Hasan <jahidmonpal911@gmail.com>
2026-08-30 15:26:16 +0600

    added html mail--but these is a issue of late response in my pc

--- task.py
+++ task.py
@@ -1,19 +1,31 @@
 import os
 import requests
 from dotenv import load_dotenv
+import jinja2
 
 load_dotenv()
 
 DOMAIN = os.getenv("MAILGUN_DOMAIN")
 
-def send_simple_message(to, subject, body):
+template_loader = jinja2.FileSystemLoader("templates")
+template_env = jinja2.Environment(loader=template_loader)
+
+
+def render_template(template_filename, **context):
+    return template_env.get_template(template_filename).render(**context)
+
+
+def send_simple_message(to, subject, body, html):
     return requests.post(
         f"https://api.mailgun.net/v3/{DOMAIN}/messages",
         auth=("api", os.getenv("MAILGUN_API_KEY")),
-        data={"from": f"Your Name <mailgun@{DOMAIN}>",
+        data={
+            "from": f"Jose Salvatierra <mailgun@{DOMAIN}>",
             "to": [to],
             "subject": subject,
-            "text": body}
+            "text": body,
+            "html": html,
+        },
     )
 
 
@@ -22,4 +34,5 @@ def send_user_registration_email(email, username):
         email,
         "Successfully signed up",
         f"Hi {username}! You have successfully signed up to the Stores REST API.",
+        render_template("email/action.html", username=username),
     )
